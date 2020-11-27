48206796c36ae027c7363b5b703e624e31589305
silin24 <silin24@foxmail.com>
2020-11-27 23:24:42 +0800

    first commit

---
+++ src/components/FooterGuide/FooterGuide.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="footer-guide">
+    <span>
+      <span>
+        <i class="iconfont icon-icon_daiyanghuconserve"></i>
+      </span>
+      <span>用户</span>
+    </span>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: "FooterGuide",
+};
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import '../../common/stylus/mixins.styl'
+</style>
